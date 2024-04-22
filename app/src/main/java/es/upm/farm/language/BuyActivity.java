8260f548af9defb4ccc6c443fe1cd759312f75f7
Nikoleta Dalaktchieva <nikoletadal@gmail.com>
2024-04-22 17:48:25 +0200

    add the basic buy screen

---
+++ app/src/main/java/es/upm/farm/language/BuyActivity.java
@@ -0,0 +1,22 @@
+package es.upm.farm.language;
+
+import android.os.Bundle;
+import android.widget.ListView;
+
+import androidx.activity.EdgeToEdge;
+import androidx.appcompat.app.AppCompatActivity;
+import androidx.core.graphics.Insets;
+import androidx.core.view.ViewCompat;
+import androidx.core.view.WindowInsetsCompat;
+
+import java.util.ArrayList;
+
+public class BuyActivity extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        EdgeToEdge.enable(this);
+        setContentView(R.layout.activity_buy);
+    }
+}
