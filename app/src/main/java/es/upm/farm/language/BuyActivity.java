4bc955cc93c883819f8defa2fa47533471d434e9
Nikoleta Dalaktchieva <nikoletadal@gmail.com>
2024-04-23 13:56:53 +0200

    Header layout

--- app/src/main/java/es/upm/farm/language/BuyActivity.java
+++ app/src/main/java/es/upm/farm/language/BuyActivity.java
@@ -2,6 +2,7 @@ package es.upm.farm.language;
 
 import android.os.Bundle;
 import android.widget.ListView;
+import android.widget.TextView;
 
 import androidx.activity.EdgeToEdge;
 import androidx.appcompat.app.AppCompatActivity;
@@ -18,5 +19,8 @@ public class BuyActivity extends AppCompatActivity {
         super.onCreate(savedInstanceState);
         EdgeToEdge.enable(this);
         setContentView(R.layout.activity_buy);
+
+        TextView headerTitle = findViewById(R.id.header).findViewById(R.id.header_title);
+        headerTitle.setText("Buy");
     }
 }
