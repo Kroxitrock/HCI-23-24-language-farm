da8ec811ed0f080e732ee30caafc2994c7367131
Nikoleta Dalaktchieva <nikoletadal@gmail.com>
2024-05-05 15:36:54 +0200

    add second ex

--- app/src/main/java/es/upm/farm/language/SpeakingExercises1.java
+++ app/src/main/java/es/upm/farm/language/SpeakingExercises1.java
@@ -1,5 +1,6 @@
 package es.upm.farm.language;
 
+import android.content.Intent;
 import android.os.Bundle;
 import android.view.View;
 import android.widget.TextView;
@@ -17,17 +18,29 @@ public class SpeakingExercises1 extends AppCompatActivity {
         findViewById(R.id.confirm_button).setOnClickListener(new View.OnClickListener() {
             @Override
             public void onClick(View v) {
-                findViewById(R.id.correct_pop_up).setVisibility(View.VISIBLE);
-                TextView result = findViewById(R.id.result);
-                TextView answer = findViewById(R.id.answer);
-                String answerStr = answer.getText().toString().toLowerCase();
-                if (answerStr.contains("de donde eres") || answerStr.contains("de dónde eres") ){
-                    result.setText("Correct answer!");
-                } else {
-                    result.setText("Wrong answer!");
-                }
-
+                showConfirmationMessage();
             }
         });
+
+
+        findViewById(R.id.btn_continue).setOnClickListener(new View.OnClickListener(){
+            @Override
+            public void onClick(View v) {
+                Intent intent = new Intent(getApplicationContext(), SpeakingExercises2.class);
+                startActivity(intent);
+            }
+        });
+    }
+
+    private void showConfirmationMessage(){
+        findViewById(R.id.correct_pop_up).setVisibility(View.VISIBLE);
+        TextView result = findViewById(R.id.result);
+        TextView answer = findViewById(R.id.answer);
+        String answerStr = answer.getText().toString().toLowerCase();
+        if (answerStr.contains("de donde eres") || answerStr.contains("de dónde eres") ){
+            result.setText("Correct answer!");
+        } else {
+            result.setText("Wrong answer!");
+        }
     }
 }
