40cb5cc4f1eab14c9ec7c19a37b220b8e4443f9d
Nikoleta Dalaktchieva <nikoletadal@gmail.com>
2024-05-05 16:04:20 +0200

    Open the camp fire from main screen

--- app/src/main/java/es/upm/farm/language/MainActivity.java
+++ app/src/main/java/es/upm/farm/language/MainActivity.java
@@ -14,9 +14,14 @@ public class MainActivity extends AppCompatActivity {
         setContentView(R.layout.activity_main);
 
         findViewById(R.id.market_button).setOnClickListener(this::openMarket);
+        findViewById(R.id.speaking_button).setOnClickListener(this::openCampFire);
     }
 
     private void openMarket(View view) {
         startActivity(new Intent(this, MarketActivity.class));
+    }
+
+    private void openCampFire(View view) {
+        startActivity(new Intent(this, CampFire.class));
     }
 }
