9e3c442284dfa8b19ef51d982d12122a968c358a
Nikoleta Dalaktchieva <nikoletadal@gmail.com>
2024-05-05 16:52:39 +0200

    Camp fire button to start ex

--- app/src/main/java/es/upm/farm/language/CampFire.java
+++ app/src/main/java/es/upm/farm/language/CampFire.java
@@ -1,6 +1,8 @@
 package es.upm.farm.language;
 
+import android.content.Intent;
 import android.os.Bundle;
+import android.view.View;
 
 import androidx.activity.EdgeToEdge;
 import androidx.appcompat.app.AppCompatActivity;
@@ -15,5 +17,11 @@ public class CampFire extends AppCompatActivity {
         super.onCreate(savedInstanceState);
         EdgeToEdge.enable(this);
         setContentView(R.layout.activity_camp_fire);
+        findViewById(R.id.start_exc).setOnClickListener(this::openSpeakingEx1);
+    }
+
+
+    private void openSpeakingEx1(View view) {
+        startActivity(new Intent(this, SpeakingExercises1.class));
     }
 }
