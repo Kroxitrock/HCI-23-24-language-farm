271d37db23921e1ff157f5b89ae259136892488a
Nikoleta Dalaktchieva <nikoletadal@gmail.com>
2024-05-05 20:41:21 +0200

    Add back btn functionality

--- app/src/main/java/es/upm/farm/language/SpeakingExercises1.java
+++ app/src/main/java/es/upm/farm/language/SpeakingExercises1.java
@@ -1,5 +1,7 @@
 package es.upm.farm.language;
 
+import android.app.AlertDialog;
+import android.content.DialogInterface;
 import android.content.Intent;
 import android.os.Bundle;
 import android.view.View;
@@ -23,24 +25,47 @@ public class SpeakingExercises1 extends AppCompatActivity {
         });
 
 
-        findViewById(R.id.btn_continue).setOnClickListener(new View.OnClickListener(){
+        findViewById(R.id.btn_continue).setOnClickListener(new View.OnClickListener() {
             @Override
             public void onClick(View v) {
                 Intent intent = new Intent(getApplicationContext(), SpeakingExercises2.class);
                 startActivity(intent);
             }
         });
+
+        findViewById(R.id.close_btn).setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+                showCloseConfirmationDialog();
+            }
+        });
     }
 
-    private void showConfirmationMessage(){
+    private void showConfirmationMessage() {
         findViewById(R.id.correct_pop_up).setVisibility(View.VISIBLE);
         TextView result = findViewById(R.id.result);
         TextView answer = findViewById(R.id.answer);
         String answerStr = answer.getText().toString().toLowerCase();
-        if (answerStr.contains("de donde eres") || answerStr.contains("de dónde eres") ){
+        if (answerStr.contains("de donde eres") || answerStr.contains("de dónde eres")) {
             result.setText("Correct answer!");
         } else {
             result.setText("Wrong answer!");
         }
+    }
+
+
+    private void showCloseConfirmationDialog() {
+        AlertDialog.Builder builder = new AlertDialog.Builder(this);
+        builder.setTitle("Confirmation");
+        builder.setMessage("Are you sure you want to cancel your speaking exercises?");
+        builder.setPositiveButton("Yes", new DialogInterface.OnClickListener() {
+            public void onClick(DialogInterface dialog, int id) {
+                finish();
+            }
+        });
+        builder.setNegativeButton("No", null);
+
+        AlertDialog dialog = builder.create();
+        dialog.show();
     }
 }
--- app/src/main/java/es/upm/farm/language/SpeakingExercises2.java
+++ app/src/main/java/es/upm/farm/language/SpeakingExercises2.java
@@ -1,5 +1,7 @@
 package es.upm.farm.language;
 
+import android.app.AlertDialog;
+import android.content.DialogInterface;
 import android.content.Intent;
 import android.os.Bundle;
 import android.view.View;
@@ -15,14 +17,40 @@ public class SpeakingExercises2 extends AppCompatActivity {
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
+
         EdgeToEdge.enable(this);
         setContentView(R.layout.activity_speaking_exercises2);
         findViewById(R.id.confirm_button).setOnClickListener(this::openCongratsScreen);
 
+        findViewById(R.id.close_btn).setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+                showCloseConfirmationDialog();
+            }
+        });
     }
 
     private void openCongratsScreen(View view) {
         startActivity(new Intent(this, SpeakingCongratsScreen.class));
+    }
+
+
+    private void showCloseConfirmationDialog() {
+        AlertDialog.Builder builder = new AlertDialog.Builder(this);
+        builder.setTitle("Confirmation");
+        builder.setMessage("Are you sure you want to cancel your speaking exercises?");
+        builder.setPositiveButton("Yes", new DialogInterface.OnClickListener() {
+            public void onClick(DialogInterface dialog, int id) {
+                Intent intent = new Intent(getApplicationContext(), CampFire.class);
+                intent.addFlags(Intent.FLAG_ACTIVITY_CLEAR_TOP | Intent.FLAG_ACTIVITY_SINGLE_TOP);
+                startActivity(intent);
+                finish();
+            }
+        });
+        builder.setNegativeButton("No", null);
+
+        AlertDialog dialog = builder.create();
+        dialog.show();
     }
 }
 
