d2606cebbd0571b1c30c156ccdeb05d8451ed344
Nikoleta Dalaktchieva <nikoletadal@gmail.com>
2024-05-05 15:25:31 +0200

    Merge with lang_exs and fix the confirm pop-up

--- app/src/main/java/es/upm/farm/language/SpeakingExercises1.java
+++ app/src/main/java/es/upm/farm/language/SpeakingExercises1.java
@@ -1,12 +1,11 @@
 package es.upm.farm.language;
 
 import android.os.Bundle;
+import android.view.View;
+import android.widget.TextView;
 
 import androidx.activity.EdgeToEdge;
 import androidx.appcompat.app.AppCompatActivity;
-import androidx.core.graphics.Insets;
-import androidx.core.view.ViewCompat;
-import androidx.core.view.WindowInsetsCompat;
 
 public class SpeakingExercises1 extends AppCompatActivity {
 
@@ -15,10 +14,20 @@ public class SpeakingExercises1 extends AppCompatActivity {
         super.onCreate(savedInstanceState);
         EdgeToEdge.enable(this);
         setContentView(R.layout.activity_speaking_exercises1);
-        ViewCompat.setOnApplyWindowInsetsListener(findViewById(R.id.main), (v, insets) -> {
-            Insets systemBars = insets.getInsets(WindowInsetsCompat.Type.systemBars());
-            v.setPadding(systemBars.left, systemBars.top, systemBars.right, systemBars.bottom);
-            return insets;
+        findViewById(R.id.confirm_button).setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+                findViewById(R.id.correct_pop_up).setVisibility(View.VISIBLE);
+                TextView result = findViewById(R.id.result);
+                TextView answer = findViewById(R.id.answer);
+                String answerStr = answer.getText().toString().toLowerCase();
+                if (answerStr.contains("de donde eres") || answerStr.contains("de dónde eres") ){
+                    result.setText("Correct answer!");
+                } else {
+                    result.setText("Wrong answer!");
+                }
+
+            }
         });
     }
 }
