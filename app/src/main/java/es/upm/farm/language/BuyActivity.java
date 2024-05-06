b4e2802735e42fbf275e9e2e52f5cda7f6e67b79
Andrey Strinski <kroxitrock@gmail.com>
2024-05-06 15:34:01 +0200

    Fix buying screen

--- app/src/main/java/es/upm/farm/language/BuyActivity.java
+++ app/src/main/java/es/upm/farm/language/BuyActivity.java
@@ -19,24 +19,34 @@ import es.upm.farm.language.models.ProductsForBuy;
 
 public class BuyActivity extends AppCompatActivity implements ProductsForBuyAdapter.OnButtonClickListener {
 
+    private Integer coins = 100;
+
     private List<ProductsForBuy> dataList;
 
     private List<ProductsForBuy> productsInCart;
     private TextView totalPrice;
 
+    private View header;
+    private TextView totalCoins;
+
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         EdgeToEdge.enable(this);
-        setContentView(R.layout.activity_buy);
+        setContentView(R.layout.activity_buy_pop_up);
 
+
+        header = findViewById(R.id.header);
         //set header title
-        TextView headerTitle = findViewById(R.id.header).findViewById(R.id.header_title);
+        TextView headerTitle = header.findViewById(R.id.header_title);
         headerTitle.setText("Buy");
+
+        totalCoins = header.findViewById(R.id.coins);
+        totalCoins.setText(Integer.toString(coins));
 
         // Set count of products
         productsInCart = new ArrayList<>();
-        totalPrice = findViewById(R.id.totalPrice);
+        totalPrice = findViewById(R.id.total_price);
         totalPrice.setText(String.valueOf(productsInCart.size()));
 
         //load the list with items for buying
@@ -48,20 +58,21 @@ public class BuyActivity extends AppCompatActivity implements ProductsForBuyAdap
 
 
         //Set buy click listener
-        findViewById(R.id.buyButton).setOnClickListener(new View.OnClickListener() {
-            @Override
-            public void onClick(View v) {
-                // Show the confirmation dialog
-                showConfirmationDialog();
-            }
+        findViewById(R.id.buy_button).setOnClickListener(v -> {
+            // Show the confirmation dialog
+            showConfirmationDialog();
+        });
+
+        findViewById(R.id.close_btn).setOnClickListener(view -> {
+            finish();
         });
     }
 
     private void loadDataList() {
-        ProductsForBuy p1 = new ProductsForBuy("Cow", 10);
+        ProductsForBuy p1 = new ProductsForBuy("Cow", 60);
         ProductsForBuy p2 = new ProductsForBuy("Chicken", 20);
-        ProductsForBuy p3 = new ProductsForBuy("Pig", 30);
-        ProductsForBuy p4 = new ProductsForBuy("Rabbit", 15);
+        ProductsForBuy p3 = new ProductsForBuy("Pig", 40);
+        ProductsForBuy p4 = new ProductsForBuy("Rabbit", 25);
         dataList = new ArrayList<>();
         dataList.add(p1);
         dataList.add(p2);
