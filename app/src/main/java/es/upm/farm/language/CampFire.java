11d12a3fe9b6e6b0429ad71625acae3d36af2ec7
Nikoleta Dalaktchieva <nikoletadal@gmail.com>
2024-05-06 21:40:53 +0200

    Add signs in the campfire

--- app/src/main/java/es/upm/farm/language/CampFire.java
+++ app/src/main/java/es/upm/farm/language/CampFire.java
@@ -2,6 +2,7 @@ package es.upm.farm.language;
 
 import android.content.Intent;
 import android.os.Bundle;
+import android.view.LayoutInflater;
 import android.view.View;
 
 import androidx.activity.EdgeToEdge;
@@ -18,10 +19,26 @@ public class CampFire extends AppCompatActivity {
         EdgeToEdge.enable(this);
         setContentView(R.layout.activity_camp_fire);
         findViewById(R.id.start_exc).setOnClickListener(this::openSpeakingEx1);
+        findViewById(R.id.market_button).setOnClickListener(this::openMarket);
+        findViewById(R.id.factory_button).setOnClickListener(this::openFactory);
+        findViewById(R.id.farm_button).setOnClickListener(this::openFarmMarket);
     }
 
 
     private void openSpeakingEx1(View view) {
         startActivity(new Intent(this, SpeakingExercises1.class));
     }
+
+    private void openMarket(View view) {
+        startActivity(new Intent(this, MarketActivity.class));
+    }
+
+    private void openFactory(View view) {
+        startActivity(new Intent(this, FirstLanguageExerciseActivity.class));
+    }
+
+    private void openFarmMarket(View view) {
+        startActivity(new Intent(this, MainActivity.class));
+    }
+
 }
